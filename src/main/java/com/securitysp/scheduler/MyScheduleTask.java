5041946aab2333fd3def8f21a09fcdbf18ddf571
kshamdal@gmail.com <kshamdal@gmail.com>
2023-09-23 23:05:56 +0530

    Schedulling is successfully working now

--- src/main/java/com/securitysp/scheduler/MyScheduleTask.java
+++ src/main/java/com/securitysp/scheduler/MyScheduleTask.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyScheduleTask {
 
-	 @Scheduled(fixedRate = 5000) // Execute every 5 seconds
+	 @Scheduled(cron="0 * * * * *") // Execute every 5 seconds
 	    public void myTask() {
 	        // Your task logic goes here
 	        System.out.println("Scheduled task executed at: " + new Date());
